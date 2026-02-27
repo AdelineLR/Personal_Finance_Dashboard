e9b931ac63a082502659f9de80e73b3d82ac62b1
Adeline Le Ray <leray.adeline@gmail.com>
2026-02-27 12:51:27 +0100

    Deletion of specific rules

--- src/categorize.py
+++ src/categorize.py
@@ -1,7 +1,7 @@
 # Author : Adeline Le Ray
 # Date : 2025/05/28
 # Project : Personal finance analysis
-# Content : Function to categorize operations and define specific rules
+# Content : Function to categorize operations in a DataFrame using regex-based matching.
 
 import pandas as pd
 import re
@@ -34,55 +34,6 @@ def categorize_operations(df, operation_col, category_rules=None):
             df.loc[mask, 'Subcategory'] = category
             df.loc[mask, 'Category'] = details['main_category']
 
-    # # Custom specific rules (if true)
-    # if specific_rules:
-    #     for condition_mask, category in specific_rules:
-    #         df.loc[condition_mask & (df['Category'] == 'Other'), 'Category'] = category
-
     return df
 
     
-# def build_specific_rules_from_json(df, rules):
-#     """
-#     Builds specific rule-based masks for categorizing operations from a JSON list of rules.
-
-#     Args:
-#         df (pd.DataFrame): The cleaned DataFrame containing bank transactions.
-#         rules (list of dict): List of rules (loaded from a JSON file), where each rule specifies:
-#             - type: Type of rule ('date_range', 'regex', 'value_match')
-#             - field: Column on which to apply the rule (for 'regex' and 'value_match')
-#             - pattern or values: Pattern(s) to match
-#             - amount: (Optional) Exact amount to match
-#             - start/end: (For 'date_range') start and end dates in string format
-#             - category: Category label to assign if rule matches
-
-#     Returns:
-#         list of tuples: Each tuple contains (boolean mask, category name)
-#     """
-#     specific_rules = []
-
-#     for rule in rules:
-#         category = rule.get("category", "Other")
-
-#         if rule["type"] == "date_range":
-#             start = pd.to_datetime(rule["start"])
-#             end = pd.to_datetime(rule["end"])
-#             mask = df["Date"].between(start, end)
-
-#         elif rule["type"] == "regex":
-#             mask = df[rule["field"]].str.contains(rule["pattern"], case=False, na=False, regex=True)
-
-#         elif rule["type"] == "value_match":
-#             values = rule['values']
-#             if isinstance(values, str):
-#                 values=[values]
-#             mask = df[rule["field"]].str.contains('|'.join(map(re.escape,values)), case=False, na=False, regex=True)
-#             if "amount" in rule:
-#                 mask &= (df["Amount"] == rule["amount"])
-
-#         else:
-#             continue
-
-#         specific_rules.append((mask, category))
-
-#     return specific_rules
